72ecae778dbaaaab861d82e43b535ef1a115ef0f
shlomi batito <shlomi.batito616@gmail.com>
2018-12-12 15:36:33 +0200

    12

--- app/src/main/java/il/co/bat/shlomi/javaproject/controller/MainActivity.java
+++ app/src/main/java/il/co/bat/shlomi/javaproject/controller/MainActivity.java
@@ -1,21 +1,31 @@
 package il.co.bat.shlomi.javaproject.controller;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
-import il.co.bat.shlomi.javaproject.R;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends Activity  implements View.OnClickListener{
@@ -46,7 +56,7 @@ public class MainActivity extends Activity  implements View.OnClickListener{
     private void findViews() {
         placeAutocompleteFragment1 = (PlaceAutocompleteFragment)getFragmentManager().findFragmentById( R.id.place_autocomplete_fragment1 );
         placeAutocompleteFragment2 = (PlaceAutocompleteFragment)getFragmentManager().findFragmentById( R.id.place_autocomplete_fragment2 );
-        button = (Button)findViewById( R.id.button );
+        button = (Button)findViewById(R.id.button );
         statusTextView = (TextView)findViewById( R.id.statusTextView );
        //two buttom of my location
         getLocationButton = (Button) findViewById(R.id.getLocationButton);
@@ -56,26 +66,7 @@ public class MainActivity extends Activity  implements View.OnClickListener{
         stopUpdateButton.setOnClickListener(this);
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-                // Called when a new location is found by the network location provider.
-                //    Toast.makeText(getBaseContext(), location.toString(), Toast.LENGTH_LONG).show();
-                locationTextView.setText(getPlace(location));////location.toString());
 
-                // Remove the listener you previously added
-                //  locationManager.removeUpdates(locationListener);
-            }
-
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-            }
-
-            public void onProviderEnabled(String provider) {
-            }
-
-            public void onProviderDisabled(String provider) {
-            }
-        };
-        //end
         button.setOnClickListener( this );
 
 
@@ -107,6 +98,68 @@ public class MainActivity extends Activity  implements View.OnClickListener{
             }
         });
     }
+
+    private void getLocation() {
+
+        //     Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 5);
+
+        } else {
+            // Android version is lesser than 6.0 or the permission is already granted.
+            stopUpdateButton.setEnabled(true);
+            getLocationButton.setEnabled(false);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+
+    }
+
+
+
+
+
+    public String getPlace(Location location) {
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+            if (addresses.size() > 0) {
+                String cityName = addresses.get(0).getAddressLine(0);
+                //  String stateName = addresses.get(0).getAddressLine(1);
+                //  String countryName = addresses.get(0).getAddressLine(2);
+                //  return stateName + "\n" + cityName + "\n" + countryName;
+                return cityName;
+            }
+
+            return "no place: \n ("+location.getLongitude()+" , "+location.getLatitude()+")";
+        }
+        catch(
+                IOException e)
+
+        {
+            e.printStackTrace();
+        }
+        return "IOException ...";
+    }
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 5) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+                stopUpdateButton.setEnabled(true);
+                getLocationButton.setEnabled(false);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+            } else {
+                Toast.makeText(this, "Until you grant the permission, we canot display the location", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -126,6 +179,15 @@ public class MainActivity extends Activity  implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
+        if (v == getLocationButton) {
+            getLocation(); // Handle clicks for getLocationButton
+        }
+        if (v == stopUpdateButton) {
+            // Remove the listener you previously added
+            locationManager.removeUpdates(locationListener);
+            stopUpdateButton.setEnabled(false);
+            getLocationButton.setEnabled(true);
+        }
 
     }
 }
